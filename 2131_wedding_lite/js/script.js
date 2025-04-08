13ef61acf8d48a991c41c80a8a9b14e765a7f2ee
Dumble@1099 <sunilkumartalluri1234@gmail.com>
2025-04-08 15:35:34 +0530

    commit

---
+++ 2131_wedding_lite/js/script.js
@@ -0,0 +1,6 @@
+console.log('JS is working!');
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(reg => console.log('Service Worker registered', reg))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  }
